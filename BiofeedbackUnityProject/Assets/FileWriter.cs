8f56c8205b04dbd757f6ce05ed304860e20509dc
strank <list-ener@strank.info>
2015-02-13 18:16:49 -0500

    Separate FileWriter into its own file

---
+++ BiofeedbackUnityProject/Assets/FileWriter.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+
+
+/// <summary>
+/// Simple file writer that puts data files in a known foder with a known suffix.
+/// </summary>
+public class FileWriter
+{
+	static public string dataPath = @"../Data/";
+	static public string dataSuffix = @".txt";
+	
+	static public void TxtSaveByStr(string saveName, string txtStr)
+	{
+		string path = dataPath + saveName + dataSuffix;
+		// This text is added only once to the file. 
+		if (!File.Exists(path)) 
+		{
+			// Create a file to write to. 
+			using (StreamWriter swrite = File.CreateText(path)) 
+			{}
+		}		
+		// This text is always added, making the file longer over time if it is not deleted. 
+		using (StreamWriter swrite = File.AppendText(path)) 
+		{
+			swrite.WriteLine(txtStr);
+		}
+	}
+	
+}
--- BiofeedbackUnityProject/Assets/Sc.cs
+++ BiofeedbackUnityProject/Assets/Sc.cs
@@ -11,32 +11,7 @@ using System.Runtime.InteropServices;
 using System.IO;
 //using System.Threading.Tasks;
 
-/// <summary>
-/// Simple file writer that puts data files in a known foder with a known suffix.
-/// </summary>
-public class FileWriter
-{
-	static public string dataPath = @"../Data/";
-	static public string dataSuffix = @".txt";
 
-	static public void TxtSaveByStr(string saveName, string txtStr)
-	{
-		string path = dataPath + saveName + dataSuffix;
-		// This text is added only once to the file. 
-		if (!File.Exists(path)) 
-		{
-			// Create a file to write to. 
-			using (StreamWriter swrite = File.CreateText(path)) 
-			{}
-		}		
-		// This text is always added, making the file longer over time if it is not deleted. 
-		using (StreamWriter swrite = File.AppendText(path)) 
-		{
-			swrite.WriteLine(txtStr);
-		}
-	}
-
-}
 
 public class Datafetch
 {
