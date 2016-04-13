dc65b269490bb0293b29b6bb403952220ef925c4
strank <list-ener@strank.info>
2016-04-12 20:41:10 -0400

    Steps towards simplifying the arduino code, 10 millisecond pause

--- Arduino-eHealth/eHealth_UNITY/eHealth_UNITY.ino
+++ Arduino-eHealth/eHealth_UNITY/eHealth_UNITY.ino
@@ -30,19 +30,15 @@
  *  Version 2.0
  *  Author: Luis Martin & Ahmad Saad
  */
-#include <PinChangeInt.h>
 #include <eHealth.h>
-int cont = 0;
+
 // The setup routine runs once when you press reset:
 void setup() {
   Serial.begin(115200);  
-  eHealth.initPulsioximeter();   
-  PCintPort::attachInterrupt(6, readPulsioximeter, RISING);
 }
 
 // The loop routine runs over and over again forever:
 void loop() {
-
   float ECG = eHealth.getECG();
   int air = eHealth.getAirFlow();
   float Air = (float)air*5/1023;
@@ -57,16 +53,6 @@ void loop() {
   Serial.print(GSR, 2); 
   Serial.print(","); 
   Serial.println(HR); 
-  delay(1);	// wait for a millisecond
+  delay(10);	// wait in milliseconds
 }
-//Include always this code when using the pulsioximeter sensor
-//=========================================================================
-void readPulsioximeter(){  
 
-  cont ++;
-
-  if (cont == 50) { //Get only of one 50 measures to reduce the latency
-    eHealth.readPulsioximeter();  
-    cont = 0;
-  }
-}
